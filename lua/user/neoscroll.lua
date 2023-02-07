5b3980f1195ad46599571ea373b127ffd1a09727
Douglas Groene <dgroene@Doug-personal-MacBook.local>
2023-02-07 07:44:08 -0500

    adds neoscroll

---
+++ lua/user/neoscroll.lua
@@ -0,0 +1,8 @@
+local status_ok, neoscroll = pcall(require, "neoscroll")
+if not status_ok then
+    return
+end
+
+require("neoscroll").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
